eb70fcc9a99c00c1732c7426b6e6c0c5dcda4da5
Tacata <933882@cognizant.com>
2023-03-06 10:26:52 +0800

    Added Assignment for Session 4

---
+++ UnitTestMasters_Session4Assignment/ProtectionAgainstRegression.cs
@@ -0,0 +1,23 @@
+﻿namespace UnitTestMasters_Session4Assignment
+{
+    public class ProtectionAgainstRegressionTests
+    {
+        [Fact]
+        public void Test()
+        {
+            // Answer here
+        }
+    }
+
+    internal class Calculator
+    {
+        public Calculator()
+        {
+        }
+
+        internal int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
